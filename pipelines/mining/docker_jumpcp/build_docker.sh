7d62f1f8bc2c4511f9eb00c332fef2ae9a3e8007
Nicole Deflaux <deflaux@google.com>
2022-11-08 12:45:01 -0800

    Fix Docker build.

--- pipelines/mining/docker_jumpcp/build_docker.sh
+++ pipelines/mining/docker_jumpcp/build_docker.sh
@@ -5,10 +5,10 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-CLOUD_BUILD_PROJECT = $1
-GCR_PROJECT = $2
+CLOUD_BUILD_PROJECT=${1}
+GCR_PROJECT=${2}
 
 gcloud --project ${CLOUD_BUILD_PROJECT} builds submit \
   --timeout 20m \
   --tag gcr.io/${GCR_PROJECT}/cytomining_jumpcp_recipe:`date +"%Y%m%d_%H%M%S"` \
-  Dockerfile.jumpcp
+  .
