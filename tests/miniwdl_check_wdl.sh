89816c9d67f8adc99ea4d6f735ff09fdf6741e7b
Stephen Fleming <sjf11@case.edu>
2023-01-09 12:54:23 -0500

    Update job name and remove obsolete comment

--- tests/miniwdl_check_wdl.sh
+++ tests/miniwdl_check_wdl.sh
@@ -3,7 +3,6 @@
 set -euxo pipefail
 
 # runs from the root directory of the repo
-# environment variable WOMTOOL_JAR is set by the github action 'install-cromwell'
 
 # find all WDL files
 WDL_FILES=$(find . -type f -name "*.wdl")
